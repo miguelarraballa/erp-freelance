84358d6188d16f176a3dba1df278a3854c697dac
Miguel Arrabal <info@mdmgdesarrolloweb.com>
2026-01-24 14:38:48 +0100

    fix (main) v1.5.0 Correccion en titulo de la página

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -10,8 +10,6 @@ use Filament\Pages\Dashboard as BaseDashboard;
 class Dashboard extends BaseDashboard
 {
     protected static BackedEnum|string|null $navigationIcon = 'heroicon-o-home';
-    protected static ?string $navigationLabel = 'Dashboard';
-    protected static ?string $title = 'Dashboard';
 
     protected function getHeaderWidgets(): array
     {
