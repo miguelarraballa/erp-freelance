fa1846b4377552542423fb3ca7ab8b9fc73fc334
Miguel Arrabal <info@mdmgdesarrolloweb.com>
2026-02-04 20:50:49 +0100

    feat (pagos_en_factura) v1.7.4 Añadido la descarga del justificante en las tablas de Pago

---
+++ resources/views/filament/partials/panel-version.blade.php
@@ -0,0 +1,3 @@
+<div class="ml-2 leading-tight text-gray-500" style="font-size: 0.75rem; margin-left: 0.5rem; margin-top: 1.25rem;">
+    v{{ config('app.version') }} · {{ config('app.release_date') }}
+</div>
